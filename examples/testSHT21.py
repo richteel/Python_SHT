9a74910e10cef112d23c8c96f2fd942395971f5d
Richard Teel <richteel@teelsys.com>
2017-11-26 16:55:41 -0500

    Corrected i2c bus in example

--- examples/testSHT21.py
+++ examples/testSHT21.py
@@ -1,6 +1,6 @@
 
 import TeelSys_SHT.SHT21 as SHT21
 
-with SHT21.SHT21(0) as sht21:
+with SHT21.SHT21(1) as sht21:
     print "Temperature: %s"%sht21.read_temperature()
     print "Humidity: %s"%sht21.read_humidity()
